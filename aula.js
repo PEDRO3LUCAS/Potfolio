ff6c343ff334f2578cc269d02449548f3e7ba059
PEDRO3LUCAS <pedrolucasspindula@gmail.com>
2025-08-08 11:27:06 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,4 @@
+function mudarMensagem() {
+    const mens = document.getElementById("mensagem");
+    mens.innerHTML = "você acabou de ganhar 5% de desconto em todos os produtos do site! <br> aproveite.";
+}
